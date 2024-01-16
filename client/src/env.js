10db10fc171cbd9152c95ca5f857d9038f86f7e4
Heather Floyd <heatherflo@gmail.com>
2024-01-16 16:28:31 -0700

    Event model set up

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-z4hmit7z70rm51po.us.auth0.com'
+export const audience = 'https://SweetSummer.com'
+export const clientId = 'OxuBQY8sEe15P1c2xINRGK8QUi5E4pGG'
